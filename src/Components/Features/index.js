b0816fa38f952b6beccbee94ad84befdea2e6ebf
Jorge Gomez <jgomezm@ucenfotec.ac.cr>
2018-05-02 13:34:13 -0600

    Finished Features componentm, import some img & Styling (Mobile first)

--- src/Components/Features/index.js
+++ src/Components/Features/index.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import iconD from '../../img/iconD.png';
+import iconP from '../../img/iconP.png';
+import iconC from '../../img/iconC.png';
 
 // Styles
 import '../../App.css';
@@ -10,8 +13,38 @@ import Screenshots from '../Screenshots';
 class Features extends Component {
   render () {
     return (
-      <div className="AddTask-form">
-        <h3>This is Features component!</h3>
+      <div className="Features-div">
+        <h1 className="Features-title">Perfect Features</h1>
+        <h2 className="Features-subtitle">Only necessary</h2>
+        <div className="row">
+          <div className="divCol4">
+            <div className="ColDiv-1">
+              <img src={iconD} className="iconD" alt="icon"/>
+            </div>
+            <div className="ColDiv-2">
+              <h3 className="Title-columns">Usability </h3>
+              <p className="Text-columns">Sometimes the simplest things are the hardest to find. So we createda new line for everyday life.</p>
+            </div>
+          </div>
+          <div className="col-sm-4 divCol4">
+            <div className="ColDiv-1">
+              <img src={iconP} className="iconP" alt="icon"/>
+            </div>
+            <div className="ColDiv-2">
+              <h3 className="Title-columns">Parallax Effect </h3>
+              <p className="Text-columns">Sometimes the simplest things are the hardest to find. So we createda new line for everyday life.</p>
+            </div>
+          </div>
+          <div className="col-sm-4 divCol4">
+            <div className="ColDiv-1">
+              <img src={iconC} className="iconC" alt="icon"/>
+            </div>
+            <div className="ColDiv-2">
+              <h3 className="Title-columns">Unlimites Colors </h3>
+              <p className="Text-columns">Sometimes the simplest things are the hardest to find. So we createda new line for everyday life.</p>
+            </div>
+          </div>
+        </div>
         <Widgets />
         <Screenshots />
       </div>
